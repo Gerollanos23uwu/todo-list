314fa3dac3dc94934110c5a99d29171c2c5d8c87
Geronimo Llanos <g.llanosfernandez@itecriocuarto.org.ar>
2024-04-25 17:29:41 -0300

    Actualizacion

--- App/app.js
+++ App/app.js
@@ -1,10 +1,8 @@
-// Info date
 const dateNumber = document.getElementById('dateNumber');
 const dateText = document.getElementById('dateText');
 const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
 
-// Tasks Container
 const tasksContainer = document.getElementById('tasksContainer');
 
 const setDate = () => {
@@ -24,6 +22,10 @@ const addNewTask = event => {
     task.addEventListener('click', changeTaskState)
     task.textContent = value;
     tasksContainer.prepend(task);
+    
+    // Guardar la tarea en localStorage
+    saveTaskToLocalStorage(value);
+    
     event.target.reset();
 };
 
@@ -44,10 +46,41 @@ const renderOrderedTasks = () => {
     order().forEach(el => tasksContainer.appendChild(el))
 }
 
-// Agregar evento para iniciar el arrastre
 tasksContainer.addEventListener('dragstart', (event) => {
     event.dataTransfer.setData('text/plain', event.target.id);
 });
 
+function addNewPerson(event) {
+    event.preventDefault();
+    var personName = event.target.personName.value;
+    
+    var personsContainer = document.getElementById('personsContainer');
+    var newPersonElement = document.createElement('div');
+    newPersonElement.textContent = personName;
+    personsContainer.appendChild(newPersonElement);
+    
+    event.target.personName.value = "";
+}
+
+// Obtener tareas almacenadas en localStorage
+const getTasksFromLocalStorage = () => {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(task => {
+        const taskElement = document.createElement('div');
+        taskElement.classList.add('task', 'roundBorder');
+        taskElement.addEventListener('click', changeTaskState);
+        taskElement.textContent = task;
+        tasksContainer.appendChild(taskElement);
+    });
+};
+
+// Guardar nueva tarea en localStorage
+const saveTaskToLocalStorage = (task) => {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
 setDate();
+getTasksFromLocalStorage();
+()localStorage.clear();
